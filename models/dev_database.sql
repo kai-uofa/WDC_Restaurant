a81de0beffb2843366ca877548e3b37034ba5741
Kai Nguyen <kainguyen@student-10-201-42-187.wireless.adelaide.edu.au>
2019-05-01 17:24:27 +0930

    - Added ER diagram - Update dev_database.sql: create tables

--- models/dev_database.sql
+++ models/dev_database.sql
@@ -3,3 +3,56 @@ OR REPLACE DATABASE 'dev_database';
 CREATE
 OR REPLACE USER 'res_admin' @'localhost' IDENTIFIED BY 'resPassword';
 GRANT ALL PRIVILEGES ON dev_database.* TO 'res_admin' @'localhost';
+USE 'dev_database';
+-- Create Restaurant table
+DROP TABLE IF EXISTS 'Restaurants';
+CREATE TABLE 'Restaurants' (
+  'restaurant_id' varchar(10) NOT NULL PRIMARY KEY,
+  'name' varchar(100),
+  'address' varchar(100),
+  'capacity' int(11)
+);
+-- Create Restaurant Open Hours table
+DROP TABLE IF EXISTS 'OpenHours';
+CREATE TABLE 'OpenHours' (
+  'restaurant_id' varchar(10) NOT NULL FOREIGN KEY REFERENCES 'Restaurants',
+  'day_of_week' int(11) NOT NULL,
+  'open_time' time,
+  'close_time' time
+);
+-- Create Customers table
+DROP TABLE IF EXISTS 'Customers';
+CREATE TABLE 'Customers' (
+  'customer_id' int(11) NOT NULL PRIMARY KEY,
+  'first_name' varchar(100),
+  'last_name' varchar(100),
+  'email' varchar(100),
+  'passwords' varchar(100)
+);
+-- Create Table Booking table
+DROP TABLE IF EXISTS 'TableBooking';
+CREATE TABLE 'TableBooking' (
+  'customer_id' int(11) NOT NULL FOREIGN KEY REFERENCES 'Customers',
+  'restaurant_id' varchar(10) NOT NULL FOREIGN KEY REFERENCES 'Restaurants',
+  'start_time' time,
+  'no_of_people' int(11),
+  'status' int(11)
+);
+-- Create Reviews table
+DROP TABLE IF EXISTS 'Reviews';
+CREATE TABLE 'Reviews' (
+  'review_id' int(11) NOT NULL PRIMARY KEY,
+  'customer_id' int(11) NOT NULL FOREIGN KEY REFERENCES 'Customers',
+  'restaurant_id' varchar(10) NOT NULL FOREIGN KEY REFERENCES 'Restaurants',
+  'content' varchar(3000)
+);
+-- Create Restaurant Manager table
+DROP TABLE IF EXISTS 'RestaurantManagers';
+CREATE TABLE 'RestaurantManagers' (
+  'manager_id' int(11) NOT NULL PRIMARY KEY,
+  'restaurant_id' varchar(10) NOT NULL FOREIGN KEY REFERENCES 'Restaurants',
+  'first_name' varchar(100),
+  'last_name' varchar(100),
+  'email' varchar(100),
+  'passwords' varchar(100)
+);
