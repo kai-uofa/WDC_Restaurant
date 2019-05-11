757ce408df5a1afb5db600825ed2b70de91e6ccd
Kevin <tangkien@Tangs-MacBook-Pro.local>
2019-05-11 17:51:29 +0930

    j

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-const SignUp = () => {
+// import React from "react";
+import React, { Component } from "react";
+// const SignUp = () => {
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  class SignUp extends Component {
+    
+    
+  render() {
   return (
-    <div class="container-fluid px-3">
-      <div class="row min-vh-100">
-        <div class="col-md-8 col-lg-6 col-xl-5 d-flex align-items-center">
-          <div class="w-100 py-5 px-md-5 px-xl-6 position-relative">
-            <div class="mb-4">
+    <div className="container-fluid px-3">
+      <div className="row min-vh-100">
+        <div className="col-md-8 col-lg-6 col-xl-5 d-flex align-items-center">
+          <div className="w-100 py-5 px-md-5 px-xl-6 position-relative">
+            <div className="mb-4">
               <h2>Sign up</h2>
             </div>
-            <form class="form-validate">
-              <div class ="form-group">
-                <label for="firstName" class="form-label">
+            <form className="form-validate" onSubmit={this.handleSubmit}>
+              <div className ="form-group">
+                <label htmlFor="firstName" class="form-label">
                   First Name
                 </label>
                 <input
@@ -18,12 +26,14 @@ const SignUp = () => {
                   id="firstName"
                   type="text"
                   placeholder="First Name"
-                  autocomplete="off"
+                  onChange={this.handleChange}
+                  autoComplete="off"
                   required
                   data-msg="Please enter your first name"
                   class="form-control"
                   /> 
               </div>
+  
               <div class ="form-group">
                 <label for="lastName" class="form-label">
                   First Name
@@ -33,22 +43,24 @@ const SignUp = () => {
                   id="lastName"
                   type="text"
                   placeholder="Last Name"
-                  autocomplete="off"
+                  onChange={this.handleChange}
+                  autoComplete="off"
                   required
                   data-msg="Please enter your last name"
                   class="form-control"
                   /> 
               </div>
               <div class="form-group">
-                <label for="loginUsername" class="form-label">
+                <label for="email" class="form-label">
                   Email Address
                 </label>
                 <input
-                  name="loginUsername"
-                  id="loginUsername"
+                  name="email"
+                  id="email"
                   type="email"
                   placeholder="name@address.com"
-                  autocomplete="off"
+                  onChange={this.handleChange}
+                  autoComplete="off"
                   required
                   data-msg="Please enter your email"
                   class="form-control"
@@ -63,11 +75,14 @@ const SignUp = () => {
                   name="loginPassword"
                   id="loginPassword"
                   placeholder="Password"
+                  onChange={this.handleChange}
                   type="password"
                   required
                   data-msg="Please enter your password"
                   class="form-control"
                 />
+                        
+                
               </div>
               <div class="form-group mb-4">
                 <label for="loginPassword2" class="form-label">
@@ -77,6 +92,7 @@ const SignUp = () => {
                   name="loginPassword2"
                   id="loginPassword2"
                   placeholder="Password"
+                  onChange={this.handleChange}
                   type="password"
                   required
                   data-msg="Please enter your password"
@@ -100,5 +116,6 @@ const SignUp = () => {
     </div>
   );
 };
+}
 
 export default SignUp;
