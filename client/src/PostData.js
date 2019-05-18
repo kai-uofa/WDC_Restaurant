bb3ef062dda3a40593d7baceadaa08b8ecbf8e2b
letiendat1502 <datlee1502@gmail.com>
2019-05-18 21:14:39 +0930

    updated sign up and search

--- client/src/PostData.js
+++ client/src/PostData.js
@@ -1,19 +1,18 @@
 export function PostData(type, userData) {
-    let BaseURL = 'https://apipaypal.9lessons.info/apipaypal/';
-    //let BaseURL = 'http://localhost/socialapi/';
-    
-    return new Promise((resolve, reject) =>{
-    fetch(BaseURL+type, {
-        method: 'POST',
-        body: JSON.stringify(userData)
+  let BaseURL = "https://apipaypal.9lessons.info/apipaypal/";
+  //let BaseURL = 'http://localhost/socialapi/';
+
+  return new Promise((resolve, reject) => {
+    fetch(BaseURL + type, {
+      method: "POST",
+      body: JSON.stringify(userData)
     })
-    .then((response) => response.json())
-    .then((res) => {
+      .then(response => response.json())
+      .then(res => {
         resolve(res);
-    })
-    .catch((error) => {
-       reject(error);
-    });
-    
-    });
-    }
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
