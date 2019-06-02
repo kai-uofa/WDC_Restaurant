f828785d378db22ec98a8005e41231dcacdb0f83
Kai Nguyen <kainguyen@student-10-201-42-067.wireless.adelaide.edu.au>
2019-06-02 13:21:16 +0930

    fix undefined token issue

--- api/app.js
+++ api/app.js
@@ -40,12 +40,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   // Express headers are auto converted to lowercase
   let token = req.headers['x-access-token'] || req.headers.authorization;
-  if (token.startsWith('Bearer ')) {
-    // Remove Bearer from string
-    token = token.slice(7, token.length);
-  }
-
-  if (token) {
+  if (token !== undefined) {
+    if (token.startsWith('Bearer ')) {
+      // Remove Bearer from string
+      token = token.slice(7, token.length);
+    }
     jwt.verify(token, config.JWT_SECRET_KEY, (err, decoded) => {
       if (err) {
         return res.json({
